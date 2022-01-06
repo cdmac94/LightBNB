08c2ef8b3b8f8fcb4bcf9758d6bcf0ee20ab70fd
cdmac94 <collinmacleod79@gmail.com>
2022-01-06 23:27:23 +0000

    Add parameters to search query

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -109,14 +109,43 @@ exports.getAllReservations = getAllReservations;
 
 const getAllProperties = function(options, limit) {
 
-  return pool
-    .query(`SELECT * 
-    FROM properties
-    LIMIT $1`, [limit])
-    .then((result) => result.rows)
-    .catch((err) => {
-      console.log(err.message);
-    });
+      const queryParams = [];
+
+      let queryString = `
+      SELECT properties.*, avg(property_reviews.rating) as average_rating
+      FROM properties
+      JOIN property_reviews ON properties.id = property_id
+      `;
+    
+      if(options.city) {
+        queryParams.push(`%${options.city}%`);
+        queryString += `WHERE city LIKE $${queryParams.length} `;
+      }
+
+      if(options.minimum_price_per_night) {
+        queryParams.push(parseInt(options.minimum_price_per_night) * 100);
+        queryString += `${queryParams.length === 1 ? 'WHERE' : 'AND'} cost_per_night > $${queryParams.length}`;
+      }
+
+      if(options.maximum_price_per_night) {
+        queryParams.push(parseInt(options.maximum_price_per_night) * 100);
+        queryString += `${queryParams.length === 1 ? 'WHERE' : 'AND'} cost_per_night < $${queryParams.length}`;
+      }
+
+      if(options.minimum_rating) {
+        queryParams.push(`${options.minimum_rating}`);
+        queryString += `${queryParams.length ===1 ? 'WHERE' : 'AND'} property_reviews.rating > $${queryParams.length}`;
+      }
+    
+      queryParams.push(limit);
+      queryString += `
+      GROUP BY properties.id
+      ORDER BY cost_per_night
+      LIMIT $${queryParams.length};
+      `;
+    
+      return pool.query(queryString, queryParams).then((res) => res.rows);
+
 };
 exports.getAllProperties = getAllProperties;
 
