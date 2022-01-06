6947a8d13772dd81b6a7c0e9dfdfc681d9102387
cdmac94 <collinmacleod79@gmail.com>
2022-01-06 03:47:30 +0000

    Add web app

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -3,44 +3,45 @@ DROP TABLE IF EXISTS properties CASCADE;
 DROP TABLE IF EXISTS property_reviews CASCADE;
 DROP TABLE IF EXISTS reservations CASCADE;
 
-
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255),
-  email VARCHAR(255),
-  password VARCHAR(255)
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE properties(
   id SERIAL PRIMARY KEY  NOT NULL,
-  owner_id INTEGER REFERENCES users.id ON DELETE CASCADE,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   title VARCHAR(255) NOT NULL,
   description TEXT,
-  cost_night INTEGER,
-  parking_spaces INTEGER,
-  bathrooms INTEGER,
-  bedrooms INTEGER,
-  thumbnail TEXT,
-  coverphoto TEXT,
-  country VARCHAR(50),
-  street VARCHAR(75),
-  city  VARCHAR(50),
-  province VARCHAR(50),
-  post_code VARCHAR(50),
-  status BOOLEAN
-);
-
-CREATE TABLE property_reviews (
-  id SERIAL PRIMARY KEY NOT NULL,
-  reservation_id INTEGER REFERENCES reservations.id ON DELETE CASCADE,
-  message TEXT,
-  rating SMALLINT CHECK (rating <= 5)
+  cost_night INTEGER NOT NULL DEFAULT 0,
+  parking_spaces INTEGER NOT NULL DEFAULT 0,
+  bathrooms INTEGER NOT NULL DEFAULT 0,
+  bedrooms INTEGER NOT NULL DEFAULT 0,
+  thumbnail TEXT NOT NULL,
+  coverphoto TEXT NOT NULL,
+  country VARCHAR(50) NOT NULL,
+  street VARCHAR(75) NOT NULL,
+  city  VARCHAR(50) NOT NULL,
+  province VARCHAR(50) NOT NULL,
+  post_code VARCHAR(50) NOT NULL,
+  active BOOLEAN NOT NULL DEFAULT TRUE
 );
 
 CREATE TABLE reservations (
   id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users.id ON DELETE CASCADE,
-  property_id INTEGER REFERENCES properties.id ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
   start_date DATE,
   end_date DATE
+);
+
+CREATE TABLE property_reviews (
+  id SERIAL PRIMARY KEY NOT NULL,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  message TEXT,
+  rating SMALLINT CHECK (rating <= 5) NOT NULL DEFAULT 0
 );
